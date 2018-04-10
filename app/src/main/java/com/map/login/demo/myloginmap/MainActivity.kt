d16b2ca670c548780fc4a055413583721e3f8c07
aidrones <35325464+aidrones@users.noreply.github.com>
2018-04-10 08:12:08 -0500

    MyLoginMap added person icon for user name and lock icon for password for activity one.
    NOTES:
     can not get login if else, and toast to word properly.

--- app/src/main/java/com/map/login/demo/myloginmap/MainActivity.kt
+++ app/src/main/java/com/map/login/demo/myloginmap/MainActivity.kt
@@ -1,47 +1,66 @@
+
+
 package com.map.login.demo.myloginmap
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View;
+import android.os.Process
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.map.login.demo.myloginmap.R.id.*
+import com.map.login.demo.myloginmap.R.id.etPassword
+import com.map.login.demo.myloginmap.R.id.etUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLogin = findViewById<Button>(R.id.btnLogin);
-        val etUser = findViewById<EditText>(R.id.etUser);
-        val etPassword = findViewById<EditText>(R.id.etPassword);
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val etUser = findViewById<EditText>(R.id.etUser)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
 
-        val btnCancel = findViewById<Button>(R.id.btnCancel);
-        //val txtView3 = findViewById<TextView>(R.id.txtView3);
-        //txtView3.setText("No of Attempts: 3");
 
-        btnLogin.setOnClickListener(View.OnClickListener() {
-                var status = if
-                    (etUser.text.toString().equals("admin") &&
-                            etPassword.text.toString().equals("Password")) "Successful Login..." else "Wrong Credentials"
-                    Toast.makeText(this,status, Toast.LENGTH_SHORT).show();
-                           // "Successful Redirecting...", Toast.LENGTH_SHORT).show();
+        btnLogin.setOnClickListener {
 
-                    val intent = Intent(this, RegistrationActivity::class.java)
-                    //intent.putExtra("Welcome Please Register Now",txtsend.text.toString())
-                    startActivity(intent)
+            var status =
+            if (etUser.text.toString().equals("admin") &&
+                    etPassword.text.toString().equals("Pass")) "Successful Login"
+            else "Login Error"
+                Toast.makeText(this, status, Toast.LENGTH_LONG).show()
+            }
+        //else {
 
-                })
+                //if (!etUser.text.toString().equals("admin") &&
+                 //       etPassword.text.toString().equals("Pass")) {
+                 //   Toast.makeText(this, "Login Error", Toast.LENGTH_LONG).show()
+                //}
+            //}
+        //})
 
-        btnCancel.setOnClickListener(View.OnClickListener() {
-            android.os.Process.killProcess(android.os.Process.myPid())
-            System.exit(1);
 
-        });
+        //val intent = Intent(this, RegistrationActivity::class.java)
+        //intent.putExtra("Welcome Please Register Now",txtsend.text.toString())
+        //startActivity(intent)
+
+        btnReset.setOnClickListener (View.OnClickListener {
+            etUser.setText("")
+            etPassword.setText("")
+            etUser.requestFocus()
+        })
+
+
+        btnCancel.setOnClickListener (View.OnClickListener {
+            Process.killProcess(Process.myPid())
+            System.exit(1)
+        })
     }
 }
+
